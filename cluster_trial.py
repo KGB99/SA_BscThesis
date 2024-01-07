3814551196247b150dff9748c35acf014f0ea689
KGB99 <kbirgi@gmail.com>
2024-01-07 20:25:35 +0100

    implemented trials for cluster using gpu

--- cluster_trial.py
+++ cluster_trial.py
@@ -3,15 +3,42 @@ import numpy as np
 import torch
 import matplotlib.pyplot as plt
 import cv2
+import argparse
+import os
+import json
 
-print("hello world")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("--labels_file", required=True, type=str)
+    parser.add_argument("--labels_dir", required=False, default="./output", type=str)
+    parser.add_argument("--images_dir", required=False, type=str)
+    args = parser.parse_args()
+    labels_file = args.labels_file
+    labels_dir = args.labels_dir
+    images_dir = args.images_dir
 
-sam = sam_model_registry["vit_h"](checkpoint="/cluster/project/infk/cvg/heinj/students/kbirgi/SA_BscThesis/sam_vit_h_4b8939.pth")
-mask_generator = SamAutomaticMaskGenerator(sam)
-exit()
-# need to create a list to iterate through for the mask generations
-for i in range(0,10):
-    image = None
-    masks = mask_generator.generate(image)
+    print("CUDA is available: ", torch.cuda.is_available())
+
+    sam = sam_model_registry["vit_h"](checkpoint="/cluster/project/infk/cvg/heinj/students/kbirgi/SA_BscThesis/sam_vit_h_4b8939.pth")
+    if torch.cuda.is_available():
+        sam.to(device="cuda")
+    mask_generator = SamAutomaticMaskGenerator(sam)
     
-    #store them somehow
+    f = open(labels_file, "r")
+    labels_dict = json.load(f)
+    f.close()
+
+    for camera in labels_dict:
+        camera_dict = labels_dict[camera]
+        for imageNr in camera_dict:
+            img_dict = camera_dict[imageNr]['img']
+            img_path = images_dir + "/" + img_dict['file_name']
+            processed_path = labels_dir + "/" + img_dict['file_name']
+            
+            image = cv2.imread(img_path)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            masks = mask_generator.generate(image)
+            print(masks)
+            exit()
+
+    print("OK!")
--- trial_script.sh
+++ trial_script.sh
@@ -2,9 +2,8 @@
 
 #SBATCH -n 1
 #SBATCH --gpus=1
-#SBATCH --mem-per-cpu=20G
+#SBATCH --mem-per-cpu=80G
 #SBATCH --time=4:00:00
-#module load gcc/8.2.0 python_gpu/3.11.2 eth_proxy
 source myenv/bin/activate
-python3 cluster_trial.py
+python3 cluster_trial.py --labels_file=/cluster/project/infk/cvg/heinj/students/kbirgi/Annotations/trainSSD/amodal_labels.json --images_dir=/cluster/project/infk/cvg/heinj/datasets/bop/mvpsp
 deactivate
