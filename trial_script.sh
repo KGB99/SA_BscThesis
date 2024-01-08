b037f0670c894f6502e9a4dc37ed642d3ac6efc9
KGB99 <kbirgi@gmail.com>
2024-01-08 13:43:50 +0100

    finalize script

--- trial_script.sh
+++ trial_script.sh
@@ -3,7 +3,7 @@
 #SBATCH -n 1
 #SBATCH --gpus=1
 #SBATCH --mem-per-cpu=80G
-#SBATCH --time=4:00:00
+#SBATCH --time=24:00:00
 source myenv/bin/activate
-python3 cluster_trial.py --labels_file=/cluster/project/infk/cvg/heinj/students/kbirgi/Annotations/trainSSD/amodal_labels.json --images_dir=/cluster/project/infk/cvg/heinj/datasets/bop/mvpsp
+python3 cluster_trial.py --labels_file=/cluster/project/infk/cvg/heinj/students/kbirgi/Annotations/trainSSD/amodal_labels.json --images_dir=/cluster/project/infk/cvg/heinj/datasets/bop/mvpsp --labels_dir=/cluster/project/infk/cvg/heinj/students/kbirgi/Annotations/trainSSD/SA_processed_images/mvpsp
 deactivate
